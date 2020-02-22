0f1b1effad6a4ca7b1828713263215a0b4be9ff3
Luke Johnson <lukerjohnson2@gmail.com>
2020-02-22 12:23:05 -0600

    updated art file.. not fully working (2-21)

--- lib/pages/art.dart
+++ lib/pages/art.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
 class Art extends StatelessWidget {
   @override
@@ -20,8 +22,91 @@ class Art extends StatelessWidget {
         ),
         body: Container(
           padding: EdgeInsets.only(left: 10.0, top: 20.0),
-          //child: jazzBodyLayout(),
+          child: artList(),
         ),
+      ),
+    );
+  }
+}
+
+class artList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(minWidth: 100, minHeight: 50),
+        child: Center(
+          child: SizedBox(
+            height: 1000,
+            width: 350,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    width: 350,
+                    height: 200,
+                    child: FloatingActionButton(
+                      heroTag: 2,
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => ArtWebView(
+                              title: "Tennessee Valley Museum of Art",
+                              selectedUrl:
+                                  "https://www.tvaa.net/visit-the-museum.html",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image.asset(
+                        'images/tvma.jpg',
+                        width: 350.0,
+                        height: 200.0,
+                        fit: BoxFit.fill,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ArtWebView extends StatelessWidget {
+  final String title;
+  final String selectedUrl;
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  ArtWebView({
+    @required this.title,
+    @required this.selectedUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.amber[100],
+      ),
+      body: WebView(
+        initialUrl: selectedUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
       ),
     );
   }
