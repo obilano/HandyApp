9ce0615ae6eecea210123cbc706da6622fbab609
Surya6032 <psurya.924@gmail.com>
2020-02-28 11:04:08 -0600

    Update 2202020

--- lib/misc/dataSearch.dart
+++ lib/misc/dataSearch.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:handy_app/pages/art.dart';
+import 'package:handy_app/pages/blues.dart';
+import 'package:handy_app/pages/events.dart';
+import 'package:handy_app/pages/food.dart';
+import 'package:handy_app/pages/jazz.dart';
+import 'package:handy_app/pages/rock.dart';
+import 'package:handy_app/pages/schedule.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
 
 class DataSearch extends SearchDelegate<String> {
-  final data = ["Jazz", "Music"];
-  final recent = ["Food", "Events"];
+  final data = ["Jazz","Food","Blues","Rock","Events","Schedule","Art"];
+  final recent = ["Food", "Jazz","Rock"];
 
   @override
   List<Widget> buildActions(BuildContext context) {
 // actions for app bar
-    return [IconButton(icon: Icon(Icons.clear), onPressed: () {})];
+    return [IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = ' ';
+    }
+    )
+    ];
   }
 
   @override
@@ -23,18 +38,77 @@ class DataSearch extends SearchDelegate<String> {
   @override
   Widget buildResults(BuildContext context) {
 // show some result based on the selection
-    return null;
+    if (query=="Jazz"){
+      return new MaterialApp(
+        home: new Jazz(),
+      );
+    }
+    else if(query=="Blues"){
+      return new MaterialApp(
+        home: new Blues(),
+      );
+    }
+    else if(query=="Art"){
+      return new MaterialApp(
+        home: new Art(),
+      );
+    }
+    else if(query=="Rock"){
+      return new MaterialApp(
+        home: new Rock(),
+      );
+    }
+    else if(query=="Events"){
+      return new MaterialApp(
+        home: new Events(),
+      );
+    }
+    else if(query=="Food"){
+      return new MaterialApp(
+        home: new Food(),
+      );
+    }
+    else if(query=="Schedule"){
+      return new MaterialApp(
+        home: new Schedule(),
+      );
+    }
+    else{
+      return Container(
+        alignment: Alignment.center,
+        child: Text(
+          'Not Found!',
+          style: TextStyle(color: Colors.black),
+          textDirection: TextDirection.ltr,
+        ),
+      );
+    }
+
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
 // show when someone searches for something
-    final suggestionList = query.isEmpty ? recent : data;
+    final suggestionList = query.isEmpty
+        ? recent
+        : data.where((p)=>p.startsWith(query)).toList();
 
     return ListView.builder(
       itemBuilder: (context, index) => ListTile(
+        onTap:()
+          {
+            showResults(context);
+          },
         leading: Icon(Icons.music_note),
-        title: Text(suggestionList[index]),
+        title: RichText(text: TextSpan(
+          text: suggestionList[index].substring(0,query.length),
+          style: TextStyle(color: Colors.black, fontWeight:  FontWeight.bold),
+          children: [
+            TextSpan(
+                text: suggestionList[index].substring(query.length),
+                style: TextStyle(color:Colors.grey))
+          ]),
+        ),
       ),
       itemCount: suggestionList.length,
     );
