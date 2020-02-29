bd8d070528684c4a0cb7983629ce7b98cd2daf39
obilano <obi.2010@live.com>
2020-02-28 22:49:54 -0600

    I made many changes that will us for later on used
    - created handywebview.dart for webviews for shorter codes
    - created eventInformation.dart for band lists, shorter code: Youtube links works, map work but can't navigate direction probably needs an API, and favorite icon is up but there still need a function that will save the favorites.
    - created events.dart to turn the data into class, still needs work
    - testing favorite icon and youtube link through jazz category
    - added playlist, map, favorite icons in jazz

---
+++ lib/misc/handywebview.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
+
+class HandyWebView extends StatelessWidget {
+  final String title;
+  final String selectedUrl;
+  Color bgrColor;
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  HandyWebView({
+    @required this.title,
+    @required this.selectedUrl,
+    @required this.bgrColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Center(
+            child: Text(title),
+          ),
+          backgroundColor: bgrColor),
+      body: WebView(
+        initialUrl: selectedUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+    );
+  }
+}
